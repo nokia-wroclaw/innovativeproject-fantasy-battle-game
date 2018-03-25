1f7591a120500de4e671deaf6d30aa1e67a8bc50
Patryk Okrasa <nieeksss@gmail.com>
2018-03-25 22:29:28 +0200

    Added assets to units

---
+++ Fantasy Battle Game/Assets/Units Assets/DragonTerrorBringer/Scripts/CameraControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour 
+{
+	public Animator anim;
+	int cameraAnim;
+	int cameraAnimBack;
+
+	void Start () 
+	{
+		anim = GetComponent<Animator>();
+		cameraAnim = Animator.StringToHash("CameraAnim");
+		cameraAnimBack = Animator.StringToHash("CameraAnimBack");
+	}
+	
+	public void CameraAnim ()
+	{
+		anim.SetTrigger(cameraAnim);
+	}
+
+	public void CameraAnimBack ()
+	{
+		anim.SetTrigger(cameraAnimBack);
+	}
+	
+}
