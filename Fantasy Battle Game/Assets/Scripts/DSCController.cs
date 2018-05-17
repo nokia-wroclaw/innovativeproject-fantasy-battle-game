b42e649bad0c185c0fd6ca23e05afffc01ff29d0
Black <freedomandjustit@gmail.com>
2018-05-17 10:07:32 +0200

    Everything seems to be working, except strange layer cake

--- Fantasy Battle Game/Assets/Scripts/DSCController.cs
+++ Fantasy Battle Game/Assets/Scripts/DSCController.cs
@@ -11,10 +11,10 @@ public class DSCController : MonoBehaviour{
     private void Start(){
         foreach (Transform child in transform){
             var obj = child.gameObject.GetComponent<Text>();
-            if (obj.name == "DSC_Name")
-                Name = obj;
-            else if (obj.name == "DSC_Brief")
-                Brief = obj;
+            
+            if (obj == null) return;
+            if (obj.name == "DSC_Name") Name = obj;
+            else if (obj.name == "DSC_Brief") Brief = obj;
             else if (obj.name == "DSC_Lore") Lore = obj;
 
             obj.text = "";
@@ -28,15 +28,16 @@ public class DSCController : MonoBehaviour{
             setStatus(description.GetName(),description.GetBrief(),description.GetLore());
         }
         else{
-            if (Name!=null && Brief!=null && Lore!=null){
-                setStatus();
-            }
+            setStatus();
         }
     }
 
     private void setStatus(String name="", String brief="", String lore=""){
-        Name.text = name;
-        Brief.text = brief;
-        Lore.text = lore;
+
+        if (Name != null && Brief != null && Lore != null){
+            Name.text = name;
+            Brief.text = brief;
+            Lore.text = lore;
+        }
     }
 }
